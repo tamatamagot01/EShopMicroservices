bd6fe508bad6a2829694d06462fa8a846fde5947
Got <itsaree.n9@gmail.com>
2026-07-10 17:55:54 +0700

    update

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Carter;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+
+            // Step 1 : create Product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            // Step 2 : save to database
+
+            // Step 3 : return create product result
+            return new CreateProductResult(Guid.NewGuid());
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
