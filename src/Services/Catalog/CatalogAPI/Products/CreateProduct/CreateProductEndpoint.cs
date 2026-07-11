5af05aa49e5cc1bfdbad33636b2c810b3014f33c
scott <itsaree.n9@gmail.com>
2026-07-12 00:45:55 +0700

    update

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using Carter;
-
-namespace CatalogAPI.Products.CreateProduct
+﻿namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
     public record CreateProductResponse(Guid Id);
@@ -9,7 +7,22 @@ namespace CatalogAPI.Products.CreateProduct
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            throw new NotImplementedException();
+            app.MapPost("/products",
+                async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
         }
     }
 }
